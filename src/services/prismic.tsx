c259063f373998eb83f6700f2b581fe4e570aec6
Guilherme Naboa <guilhermenaboa31@gmail.com>
2021-10-21 15:42:06 -0300

    Update prismic.tsx

--- src/services/prismic.tsx
+++ src/services/prismic.tsx
@@ -3,7 +3,8 @@ import Prismic from '@prismicio/client'
 import { DefaultClient } from '@prismicio/client/types/client';
 
 export function getPrismicClient(req?: unknown): DefaultClient {
-  const prismic = Prismic.client(process.env.NEXT_PUBLIC_PRISMIC_API_ENDPOINT, {
+  const prismic = Prismic.client('https://worldtripapp.cdn.prismic.io/api/v2'
+, {
     req,
      accessToken: process.env.NEXT_PUBLIC_PRISMIC_ACESS_TOKEN
   });
